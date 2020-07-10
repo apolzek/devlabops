f269124d550e4938a8b38fb1f48a4b7c5106cd37
apolzek <viniciusgb@unipam.edu.br>
2020-07-10 01:07:42 -0300

    Rename API, clean repo

--- kubernetes-scale.sh
+++ kubernetes-scale.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-#PROBLEMS=$(kubectl get deployments | grep 0/0 | awk {'print $1'})
-PROBLEMS=$(kubectl get deployments | awk {'print $1'})
-for OUTPUT in $PROBLEMS
+#ITEMS=$(kubectl get deployments | grep 0/0 | awk {'print $1'})
+
+ITEMS=$(kubectl get deployments | awk {'print $1'})
+for OUTPUT in $ITEMS
 do
     kubectl scale deploy $OUTPUT --replicas=0
 done
 
-for OUTPUT in $PROBLEMS
+for OUTPUT in $ITEMS
 do
     kubectl scale deploy $OUTPUT --replicas=1
 done
